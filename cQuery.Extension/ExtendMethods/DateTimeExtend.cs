779863a14be7698a47698939f59dcecdec5a7a87
marvin <starolpboy@126.com>
2018-08-22 19:09:13 +0800

    create solution

---
+++ cQuery.Extension/ExtendMethods/DateTimeExtend.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cQuery.Extension.ExtendMethods
+{
+    public static class DateTimeExtend
+    {
+        /// <summary>
+        /// 将时间转换为字符串
+        /// </summary>
+        /// <param name="d">要转换的时间</param>
+        /// <param name="format">表示时间的格式</param>
+        /// <returns>表示时间的字符串</returns>
+        public static string ToStringIfNullThenEmpty(this DateTime? d,string format)
+        {
+            if (d.HasValue)
+            {
+                return d.Value.ToString(format);
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
---
+++ cQuery.Extension/ExtendMethods/DoubleExtend.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cQuery.Extension.ExtendMethods
+{
+    public static class DoubleExtend
+    {
+        /// <summary>
+        /// 得到double类型变量的百分比表示
+        /// </summary>
+        /// <param name="d">要转换的double类型变量</param>
+        /// <param name="digits">小数精度</param>
+        /// <returns>double类型变量的百分比表示</returns>
+        public static string ToPercentage(this double d,int digits)
+        {
+            return Math.Round(d * 100, digits) + "%";
+        }
+    }
+}
