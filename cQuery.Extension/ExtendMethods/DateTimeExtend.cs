a61eae2d47a1e2d59ec0eca4a887e40895016f66
marvin <starolpboy@126.com>
2018-10-17 15:24:54 +0800

    add extend method to DateTime

--- cQuery.Extension/ExtendMethods/DateTimeExtend.cs
+++ cQuery.Extension/ExtendMethods/DateTimeExtend.cs
@@ -25,5 +25,39 @@ namespace cQuery.Extension.ExtendMethods
                 return string.Empty;
             }
         }
+
+        /// <summary> 
+        /// 计算某时间对应礼拜一的时间
+        /// </summary> 
+        public static DateTime GetMondayDate(this DateTime someDate)
+        {
+            int i = someDate.DayOfWeek - DayOfWeek.Monday;
+            if (i == -1) i = 6;// i值 > = 0 ，因为枚举原因，Sunday排在最前，此时Sunday-Monday=-1，必须+7=6。 
+            return someDate.AddDays(-1 * i).Date;
+        }
+        /// <summary> 
+        /// 计算时间对应礼拜日的时间
+        /// </summary> 
+        public static DateTime GetSundayDate(this DateTime someDate)
+        {
+            int i = someDate.DayOfWeek - DayOfWeek.Sunday;
+            if (i != 0) i = 7 - i;// 因为枚举原因，Sunday排在最前，相减间隔要被7减。
+            return someDate.AddDays(i).Date;
+        }
+
+        /// <summary> 
+        /// 计算某时间对应当月1号的时间
+        /// </summary> 
+        public static DateTime GetFirstDayOfCurrentMonth(this DateTime someDate)
+        {
+            return someDate.AddDays(-1 * someDate.Day + 1).Date;
+        }
+        /// <summary> 
+        /// 计算某时间对应当月最后一天的时间
+        /// </summary> 
+        public static DateTime GetLastDayOfCurrentMonth(this DateTime someDate)
+        {
+            return someDate.AddDays(-1 * someDate.Day + 1).AddMonths(1).AddDays(-1).Date;
+        }
     }
 }
