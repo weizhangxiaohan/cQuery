592a08caf8f02c75c86c75badbf24fa4f349c7c0
marvin <starolpboy@126.com>
2018-11-22 11:36:46 +0800

    add test of enum extend methods

---
+++ cQuery.Extension45.Test/ExtendMethodTest/EnumTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using cQuery.Extension.ExtendMethods;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace cQuery.Extension45.Test.ExtendMethodTest
+{
+    [TestClass]
+    public class EnumTest
+    {
+        [TestMethod]
+        public void GetDescriptionTest()
+        {
+            string s = Gender.Male.GetDescription();
+            Assert.AreEqual("Man" , s);
+
+            string ss = Gender.Female.GetDescription();
+            Assert.AreEqual("Woman" , ss);
+        }
+    }
+
+    public enum Gender
+    {
+        [System.ComponentModel.Description("Man")]
+        Male = 1,
+        [System.ComponentModel.Description("Woman")]
+        Female
+    }
+}
